40ce55e1f2190470c3834bf539a5840a2ea09108
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-11-16 11:06:59 +0200

    made an interface of items

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@
  */
 
 
+import cors from 'cors'
 import * as dotenv from 'dotenv'
+import express from 'express'
+import helmet from 'helmet'
 
 dotenv.config()
 
@@ -14,4 +17,25 @@ dotenv.config()
 
 if (!process.env.port) process.exit(1)
 
-const PORT: number = parseInt(process.env.port)
+const PORT: number = parseInt(process.env.port as string, 10)
+
+const app = express()
+
+
+
+/**
+ * App configuration
+ */
+
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+
+
+/**
+ * Server Activation
+ */
+
+app.listen(() => {
+    console.log(`Listening on port ${PORT}`)
+})
